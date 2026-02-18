637de41aae907767f64d69e6d10733e06ddadd29
cybersoul21 <wilson.jar21@gmail.com>
2026-02-18 19:39:52 +0100

    base for class robot completed

--- 01_rendezvous/robot.py
+++ 01_rendezvous/robot.py
@@ -1,8 +1,14 @@
 class robot:
-    def __init__(self):
-        self.x
-        self.y
-        pass
+    def __init__(self,_id,posx=0,posy=0,n=None):
+
+        if n is None:
+            n = []  
+
+        self.id = _id
+        self.x = posx
+        self.y = posy
+        self.neighbors = n
+
 
     def __str__(self):
         pass
@@ -13,15 +19,29 @@ class robot:
     def setY(self,y):
         self.y = y   
 
-    def gettX(self):
+    def getX(self):
         return self.x
 
     def getY(self):
-        return self.y                
-        
+        return self.y
 
+    # Complete list 
+    def setNeighbors(self,nodes):
+        self.neighbors = nodes
 
+    # add one robot (node)
+    def addNode(self,node):
+        self.neighbors.append(node)
 
+    # remove one robot (node)
+    def removeNode(self,node):
+        if node in self.neighbors:
+            self.neighbors.remove(node)
+        else:
+            print(f"Element {node} not found in the list.")
+                   
+
+#TODO: Test class,  add comments, and methods to plot..or create another class to plot?
 # Test class if this script is executed directly
 def main():
     # Create an instance of the Calculator class
