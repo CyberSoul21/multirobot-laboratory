b30808d3f82674e9df34fbc165082124bb82bdcd
cybersoul21 <wilson.jar21@gmail.com>
2026-02-18 18:34:58 +0100

    template for class

---
+++ 01_rendezvous/robot.py
@@ -0,0 +1,18 @@
+class robot:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
+
+# Test class if this script is executed directly
+def main():
+    # Create an instance of the Calculator class
+    robot0 = robot()
+
+
+#Test class
+if __name__ == "__main__":
+    main()    
