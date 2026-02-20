e3bc04f75e03006a65fd0536b2d559de303e8ab6
alvaroprovencio <alvaroproven@gmail.com>
2026-02-20 10:09:07 +0100

    Threads

--- 01_rendezvous/Robot.py
+++ 01_rendezvous/Robot.py
@@ -2,7 +2,7 @@ import random
 import threading
 
 class Robot:
-    def __init__(self,_id,posx=0,posy=0, line = False,n=None):
+    def __init__(self,_id,posx=0,posy=0, bx=0, by=0, n=None):
 
         if n is None:
             n = []  
@@ -11,10 +11,12 @@ class Robot:
         self.x = posx
         self.y = posy
         self.neighbors = n
-        self.line = line
-        self.t_local = random.random(0.1,0.5)
+        self.bx = bx
+        self.by = by
+        self.t_local = random.choice([0.3, 0.6, 0.9, 1.2, 1.5])
+        # self.t_local = random.choice([1, 1.25, 1.5, 1.75, 2])
         # Manage concurency
-        self.lock = threading.lock()
+        self.lock = threading.RLock() # Allows the same thread to reenter in the lock
         self.available = True
         self.condition = threading.Condition(self.lock)
 
@@ -22,11 +24,13 @@ class Robot:
         return f"Robot {self.id} -> ({self.x:.2f}, {self.y:.2f})"
 
     def set_pos(self,x,y):
-        self.x = x
-        self.y = y 
+        with self.lock:
+            self.x = x
+            self.y = y 
 
     def get_pos(self):
-        return self.x, self.y
+        with self.lock:
+            return self.x, self.y
  
     def setNeighbors(self,nodes):
         self.neighbors = nodes
@@ -41,46 +45,20 @@ class Robot:
             print(f"Element {node} not found in the list.")
 
     def gossip_step(self):
+        if not self.neighbors:
+            return
+        
         neighbor = random.choice(self.neighbors)
-        with neighbor.lock:
-            with self.lock:
-                if self.line:
-                    self.gossip_consensus
+        first, second = (self, neighbor) if self.id < neighbor.id else (neighbor, self)
 
-
-    # def gossip_update(self):
-    #     if not self.neighbors:
-    #         return
-        
-    #     # Gossip rule
-    #     neighbour = random.choice(self.neighbors)
-    #     nx,ny = neighbour.get_pos()
-    #     new_x = (nx + self.x) / 2
-    #     new_y = (ny + self.y) / 2
-        
-    #     # Update
-    #     self.set_pos(new_x, new_y)
-    #     neighbour.set_pos(new_x, new_y)      
-
-    # def gossip_line(self, bx, by):
-    #     if not self.neighbors:
-    #         return
-        
-    #     # Gossip rule
-    #     neighbour = random.choice(self.neighbors)
-    #     nx,ny = neighbour.get_pos()
-    #     new_x = ((nx - bx*neighbour.id) + (self.x - bx*self.id)) / 2
-    #     new_y = ((ny - by*neighbour.id) + (self.y - by*self.id)) / 2
-        
-    #     # Update
-    #     self.set_pos(new_x + bx*self.id, new_y + by*self.id)
-    #     neighbour.set_pos(new_x + bx*neighbour.id, new_y + by*neighbour.id)         
+        with first.lock:
+            with second.lock:
+                # Gossip rule
+                nx,ny = neighbor.get_pos()
+                new_x = ((nx - self.bx*neighbor.id) + (self.x - self.bx*self.id)) / 2
+                new_y = ((ny - self.by*neighbor.id) + (self.y - self.by*self.id)) / 2
+                
+                # Update
+                self.set_pos(new_x + self.bx*self.id, new_y + self.by*self.id)
+                neighbor.set_pos(new_x + self.bx*neighbor.id, new_y + self.by*neighbor.id)      
                     
-
-def main():
-    robot0 = Robot()
-
-
-#Test class
-if __name__ == "__main__":
-    main()    
--- 01_rendezvous/rendezvous.py
+++ 01_rendezvous/rendezvous.py
@@ -10,32 +10,31 @@ import time
 def robot_thread(robot, t_total):
     start_time = time.time()
     actual_time = start_time
+    last_call = start_time
     while actual_time - start_time < t_total:
         actual_time = time.time()
         if actual_time - last_call > robot.t_local:
             last_call = actual_time
             robot.gossip_step()
+            # time.sleep(0)
 
 
 if __name__ == "__main__":
     # Initial params
     Num_robots = 6
-    iterations = 100
-    t_total = 20
-    line_set = True
-    # line_set = False
-    bx = 0.5
-    by = 2
+    t_total = 15
+    bx = 1
+    by = 1
 
     # Graph setup
     communication_graph = np.array([[5, 1], [0, 2], [1, 3], [2, 4],
                                     [3, 5], [4, 0], [1, 4], [5, 0]])
                                     # [3, 5], [4, 0]]) # 2 disconnected graphs 
-    undirected = True
-    alpha = 0.125
-    my_graph = graph(Num_robots, communication_graph, undirected)
-    my_graph.define_W_matrix(alpha)
-    my_graph.eigenvals()
+    # undirected = True
+    # alpha = 0.125
+    # my_graph = graph(Num_robots, communication_graph, undirected)
+    # my_graph.define_W_matrix(alpha)
+    # my_graph.eigenvals()
     # my_graph.plot_graph()
 
     # Robot declaration and neighbours setup
@@ -44,7 +43,7 @@ if __name__ == "__main__":
         # Random initial positions
         x = random.randint(0, 10)
         y = random.randint(0, 10)
-        robots.append(rbt(i, x, y, line = line_set))
+        robots.append(rbt(i, x, y, bx, by))
 
     for i, j in communication_graph:
         robots[i].addNode(robots[j])
@@ -56,36 +55,11 @@ if __name__ == "__main__":
     for robot in robots:
         print(robot)
 
-
-    # Interactive plot initialization
-    plt.ion()   # interactive mode ON
-    fig, ax = plt.subplots()
-
-    x0 = [r.x for r in robots]
-    y0 = [r.y for r in robots]
-    init_scat = ax.scatter(x0, y0, marker='x',alpha=0.6)
-    scat = ax.scatter(x0, y0)
-
-    labels = []
-    for r in robots:
-        t = ax.text(r.x+0.1, r.y+0.1, f'{r.id}', fontsize=8)
-        labels.append(t)
-
-    ax.set_xlim(-5,15)
-    ax.set_ylim(-5,15)
-    ax.set_title("Gossip Consensus")
-    plt.draw()
-    plt.pause(0.1)
-
     # Trail with past steps
     history = {r.id: [(r.x, r.y)] for r in robots}
-    lines = {}
+    x0 = [r.x for r in robots]
+    y0 = [r.y for r in robots]
 
-    for r in robots:
-        line, = ax.plot([],[],
-                        linewidth=1,
-                        alpha=0.2)
-        lines[r.id] = line
 
     # Launch threads
     threads = []
@@ -94,36 +68,77 @@ if __name__ == "__main__":
         t.start()
         threads.append(t)
 
-
     # Consensus process
-    for iteration in range(iterations):
-        robot = random.choice(robots)
+    start_time = time.time()
+    actual_time = start_time
+    T_plot = 0.05   # periodo de muestreo
 
-        if line_set: # To create a line
-            robot.gossip_line(bx,by)
-        else: # To meet at center
-            robot.gossip_update()
+    while actual_time - start_time < t_total:
+        # print(actual_time - start_time)
+        time.sleep(T_plot)
+        actual_time = time.time()
 
         # Update interactive plot
-        xs = [r.x for r in robots]
-        ys = [r.y for r in robots]
-        scat.set_offsets(list(zip(xs,ys)))
+        current_positions = [r.get_pos() for r in robots]
 
-        for r,t in zip(robots,labels):
-            history[r.id].append((r.x,r.y))
-            t.set_position((r.x+0.1,r.y+0.1))
-            hx = [p[0] for p in history[r.id]]
-            hy = [p[1] for p in history[r.id]]
-            lines[r.id].set_data(hx,hy)
+        xs, ys = [], []
+        for i, (curr_x, curr_y) in enumerate(current_positions):
+            r_id = robots[i].id
+            history[r_id].append((curr_x,curr_y))
 
-        fig.canvas.draw_idle()
-        plt.pause(0.05)
 
     # Join threads
     for t in threads:
         t.join()
 
-    # Freeze last plot
     print("-----End-----")
+    for robot in robots:
+        print(robot)
+
+    # Plot the sequence
+    plt.ion()
+    n_steps = len(next(iter(history.values())))
+
+    fig, ax = plt.subplots()
+    init_scat = ax.scatter(x0, y0, marker='x',alpha=0.6)
+    scat = ax.scatter(x0, y0)
+
+    labels = []
+    for r in robots:
+        l = ax.text(r.x+0.1, r.y+0.1, f'{r.id}', fontsize=8)
+        labels.append(l)
+
+    ax.set_xlim(-5,15)
+    ax.set_ylim(-5,15)
+    ax.set_title("Gossip Consensus")
+    plt.draw()
+    plt.pause(0.1)
+
+    # Trail with past steps
+    lines = {}
+    for r in robots:
+        line, = ax.plot([],[],
+                        linewidth=1,
+                        alpha=0.2)
+        lines[r.id] = line
+
+    for k in range(n_steps):
+        time.sleep(T_plot)
+
+        xs, ys = [], []
+        for i, r in enumerate(robots):
+            xk, yk = history[r.id][k]
+            xs.append(xk)
+            ys.append(yk)
+
+            labels[i].set_position((xk+0.1, yk+0.1))
+            hx = [p[0] for p in history[r.id][:k]]
+            hy = [p[1] for p in history[r.id][:k]]
+            lines[r.id].set_data(hx, hy)
+
+        scat.set_offsets(list(zip(xs,ys)))
+        fig.canvas.draw_idle()
+        plt.pause(T_plot)
+
     plt.ioff()
     plt.show()
